b070ff368bdccef89323fde128b88ef7e6e0085b
Nick Jones <nick@itsnotworking.co.uk>
2015-02-10 13:14:42 +0000

    Fixing circular imports

--- src/fobi/contrib/plugins/form_handlers/db_store/models.py
+++ src/fobi/contrib/plugins/form_handlers/db_store/models.py
@@ -17,31 +17,33 @@ from django.db import models
 # ****************************************************************************
 # **************** Safe User import for Django > 1.5, < 1.8 ******************
 # ****************************************************************************
-try:
-    # Django 1.7 check
-    from django.apps import AppConfig
-    from django.conf import settings
-    User = settings.AUTH_USER_MODEL
-except ImportError:
-    # Django 1.6 check
-    try:
-        from django.contrib.auth import get_user_model
-    # Fall back to Django 1.5
-    except ImportError:
-        from django.contrib.auth.models import User
-    else:
-        User = get_user_model()
+# try:
+#     # Django 1.7 check
+#     from django.apps import AppConfig
+#     from django.conf import settings
+#     User = settings.AUTH_USER_MODEL
+# except ImportError:
+#     # Django 1.6 check
+#     try:
+#         from django.contrib.auth import get_user_model
+#     # Fall back to Django 1.5
+#     except ImportError:
+#         from django.contrib.auth.models import User
+#     else:
+#         User = get_user_model()
+#
+#     # Sanity checks
+#     user = User()
+#
+#     if not hasattr(user, 'username'):
+#         from fobi.exceptions import ImproperlyConfigured
+#         raise ImproperlyConfigured("Your custom user model ({0}.{1}) doesn't "
+#                                    "have ``username`` property, while "
+#                                    "``django-fobi`` relies on its' presence"
+#                                    ".".format(user._meta.app_label, \
+#                                               user._meta.object_name))
 
-    # Sanity checks
-    user = User()
-
-    if not hasattr(user, 'username'):
-        from fobi.exceptions import ImproperlyConfigured
-        raise ImproperlyConfigured("Your custom user model ({0}.{1}) doesn't "
-                                   "have ``username`` property, while "
-                                   "``django-fobi`` relies on its' presence"
-                                   ".".format(user._meta.app_label, \
-                                              user._meta.object_name))
+user_model_label = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 # ****************************************************************************
 # ****************************************************************************
@@ -55,7 +57,7 @@ class SavedFormDataEntry(models.Model):
     """
     form_entry = models.ForeignKey('fobi.FormEntry', verbose_name=_("Form"),
                                    null=True, blank=True)
-    user = models.ForeignKey(User, verbose_name=_("User"), null=True,
+    user = models.ForeignKey(user_model_label, verbose_name=_("User"), null=True,
                              blank=True)
     form_data_headers = models.TextField(_("Form data headers"), null=True,
                                          blank=True)
